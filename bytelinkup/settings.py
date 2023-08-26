8a32b2b59d1efd258e4d6b7a065d9cdeab25e783
BaBa7080 <abhishekbaba7080@gmail.com>
2023-08-27 02:04:04 +0530

    heroku commit version

--- bytelinkup/settings.py
+++ bytelinkup/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contact.apps.ContactConfigConfig'
+    'contact.apps.ContactConfig'
 ]
 
 MIDDLEWARE = [
