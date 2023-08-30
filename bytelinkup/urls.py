dd73d19d3aefd04cc2179bc412844ed44e9f397b
BaBa7080 <abhishekbaba7080@gmail.com>
2023-08-31 03:47:07 +0530

    bug

--- bytelinkup/urls.py
+++ bytelinkup/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('contactUs/',contactus,name='contactUs'),
     # path('contact/',contact,name='contact'),
     path('services/',services,name='services'),
+    path('usereview/',ReviewClient,name='usereview'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
